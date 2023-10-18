8ce3a4ddd57daeb21f3b2b2538606646d9c7308c
Leon Husmann <git@leonhusmann.com>
2023-10-18 19:36:32 +0200

    add code style

--- src/Sniffs/BlankLineBeforeStatement.php
+++ src/Sniffs/BlankLineBeforeStatement.php
@@ -7,21 +7,27 @@ namespace CodesnifferRules\Sniffs;
 use PHP_CodeSniffer\Files\File;
 use PHP_CodeSniffer\Sniffs\Sniff;
 
+use const T_BREAK;
+use const T_WHITESPACE;
+
 class BlankLineBeforeStatement implements Sniff
 {
-    public function register()
+    /** @return list<int> */
+    public function register(): array
     {
         return [T_BREAK];
     }
 
-    public function process(File $phpcsFile, $stackPtr)
+    public function process(File $phpcsFile, mixed $stackPtr): void
     {
-        $tokens = $phpcsFile->getTokens();
-        $prevToken = $phpcsFile->findPrevious(T_WHITESPACE, ($stackPtr - 1), null, true);
+        $tokens    = $phpcsFile->getTokens();
+        $prevToken = $phpcsFile->findPrevious(T_WHITESPACE, $stackPtr - 1, null, true);
 
-        if ($tokens[$prevToken]['line'] === $tokens[$stackPtr]['line']) {
-            $error = 'Expected 1 newline before break statement; 0 found';
-            $phpcsFile->addError($error, $stackPtr, 'MissingNewline');
+        if ($tokens[$prevToken]['line'] !== $tokens[$stackPtr]['line']) {
+            return;
         }
+
+        $error = 'Expected 1 newline before break statement; 0 found';
+        $phpcsFile->addError($error, $stackPtr, 'MissingNewline');
     }
 }
--- tests/TestHelper.php
+++ tests/TestHelper.php
@@ -4,20 +4,25 @@ declare(strict_types=1);
 
 namespace CodesnifferRulesTest;
 
+use function is_array;
+
 final class TestHelper
 {
-    static function extractMessage(array $errors): string|null
+    /** @param list<mixed> $errors */
+    public static function extractMessage(array $errors): string|null
     {
         foreach ($errors as $key => $value) {
             if ($key === 'message') {
                 return $value;
             }
 
-            if (is_array($value)) {
-                $result = self::extractMessage($value);
-                if ($result !== null) {
-                    return $result;
-                }
+            if (! is_array($value)) {
+                continue;
+            }
+
+            $result = self::extractMessage($value);
+            if ($result !== null) {
+                return $result;
             }
         }
 
